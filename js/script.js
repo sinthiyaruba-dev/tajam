d67bad6964972e81468d8511a100dd75a7afd2cd
Sinthiya Akter Ruba <132220392+sinthiyaruba-dev@users.noreply.github.com>
2023-04-30 23:49:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready (function(){
+
+    $('.banner_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+      });
+      $('.slider_text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+      });
+
+});
